6b0978616f0358a16f1e474038ef03e5459059ff
DuongVM1 <99791275+DuongVM1@users.noreply.github.com>
2022-05-12 21:12:26 +0700

    add sample reactnative form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    ///
+  ]
 })
-export class AppComponent {
-  title = 'my_app';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private destroy$ = new Subject<void>();
+
+  messageErrors: any = {
+    required: 'con cho dien du lieu vao',
+    minlength: 'min length'
+  };
+
+  public fieldNameKeys = {
+    username: 'username',
+    password: 'password',
+    confirmPassword: 'confirmPassword'
+  };
+
+  public formGroup!: FormGroup;
+
+  constructor(private builder: FormBuilder) {
+
+  }
+  ngOnDestroy(): void {
+    const seft = this;
+    seft.destroy$.next();
+    seft.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    const seft = this;
+    seft.buildForm();
+  }
+
+  private buildForm() {
+    const seft = this;
+    seft.formGroup = seft.builder.group(
+      {
+        [seft.fieldNameKeys.username]: [{
+          value: '',
+          disabled: false
+        }, [
+          Validators.required,
+          Validators.minLength(2)
+        ]],
+        password: ['', [Validators.required, seft.validateMarchingPassword(seft.fieldNameKeys)]],
+        confirmPassword: ['', [Validators.required, seft.validateMarchingPassword(seft.fieldNameKeys)]]
+      }
+    );
+  }
+
+  validate (control: AbstractControl) {
+    return {
+      abc: true
+    }
+    return null;
+  }
+
+  private validateMarchingPassword(fieldNameKeys: any) {
+    return (control: AbstractControl) => {
+      if (!!control.parent?.controls) {
+        const _formGroup = control.parent as FormGroup;
+        const passwordControl = _formGroup.get(fieldNameKeys.password);
+        const confirmPasswordControl = _formGroup.get(fieldNameKeys.confirmPassword);
+        if (!!passwordControl?.value && !!confirmPasswordControl?.value && passwordControl?.value !== confirmPasswordControl?.value) {
+          return {
+            match: true
+          };
+        }
+      }
+      return null;
+    }
+  }
+
+  public getErrorField(fieldName: string): string[] {
+    const seft = this;
+    const errorObj = seft.formGroup.get(fieldName)?.errors;
+    if (!errorObj) {
+      return [];
+    }
+    const errorKeys = Object.keys(errorObj || {});
+    if (errorKeys.length === 0) {
+      return [];
+    }
+    const listMsg = errorKeys.reduce(
+      (res: string[], key: string) => {
+        const msg = seft.messageErrors[key];
+        res.push(msg);
+        return res;
+      }, []
+    );
+    // get message
+    return listMsg;
+  };
+
+  public isInvalidControl(controlName: string) {
+    const seft = this;
+    const control = seft.formGroup.get(controlName);
+    if (!control) {
+      return false;
+    }
+    return control.invalid && (control.touched || control.dirty);
+  }
+
+  public onSubmit() {
+    const seft = this;
+    seft.formGroup.valueChanges.pipe(
+      takeUntil(seft.destroy$)
+    ).subscribe(
+
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
